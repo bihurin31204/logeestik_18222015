e2a22c13e59c74b27c89ec0975d86fd9cf0fd60b
Bihurin Salsabila <bihurinsalsabilafirdaus@gmail.com>
2024-11-12 08:16:32 +0700

    revisi 3

--- app/main.py
+++ app/main.py
@@ -6,21 +6,21 @@ from datetime import datetime
 from pydantic import BaseModel
 
 app = FastAPI(
-    title="logeestik",
-    description="delivery package",
+    title="Logeestik API",
+    description="Layanan antar barang logistik",
     version="1.0",
     contact={
-        "name": "bihurin",
-        "email": "bihurin@gmail.com"
+        "name": "Bihurin",
+        "email": "bihurin18222015@example.com"
     }
 )
 
 @app.get("/")
 def read_root():
     return {
-        "project": "Logee API",
+        "project": "Logeestik API",
         "version": "1.0",
-        "developer": "bihurin",
-        "description": "delivery package",
+        "developer": "Bihurin",
+        "description": "Layanan antar barang logistik",
         "last_updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-}
+    }
